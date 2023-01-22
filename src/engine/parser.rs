1609ca18b5c3315fe6800439aad518ec5ba82cdb
mh-mobile <mh.mobiler@gmail.com>
2023-01-22 15:56:38 +0900

    抽象構文木を表現するための型を追加

--- src/engine/parser.rs
+++ src/engine/parser.rs
@@ -5,3 +5,12 @@ use std::{
     mem::take
 };
 
+//! 抽象構文木を表現するための型
+pub enum AST {
+    Char(char),
+    Plus(Box<AST>),
+    Star(Box<AST>),
+    Question(Box<AST>),
+    Or(Box<AST>, Box<AST>),
+    Seq(Vec<AST>)
+}
