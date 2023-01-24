3fea72be83b34306f2a746d0e3804284f761f696
mh-mobile <mh.mobiler@gmail.com>
2023-01-24 18:15:10 +0900

    evalメソッドを追加

--- src/engine/evaluator.rs
+++ src/engine/evaluator.rs
@@ -23,6 +23,21 @@ impl Display for EvalError {
 
 impl Error for EvalError {}
 
+/// 命令列の評価を行う関数。
+///
+/// instが命令列となり、その命令列を用いて入力文字列lineにマッチさせる。
+/// is_depthがtrueの場合に深さ優先探索を、falseの場合に幅優先探索を行う。
+///
+/// 実行時エラーが起きた場合はErrを返す。
+/// マッチ成功時はOk(true)を、失敗時はOk(false)を返す。
+pub fn eval(inst: &[Instruction], line: &[char], is_depth: bool) -> Result<bool, EvalError> {
+    if is_depth {
+        eval_depth(inst, line, 0, 0)
+    } else {
+        eval_width(inst, line)
+    }
+}
+
 /// 深さ優先探索で再起的にマッチングを行う評価器
 fn eval_depth(
     inst: &[Instruction],
